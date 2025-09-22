da7d9ffea8a3e4c11c533a48b98d5a112e46b14f
GreenKookie56 <brandon05121@gmail.com>
2025-09-22 19:27:55 +0800

    nerf the fungus

--- jokers/thefungus.lua
+++ jokers/thefungus.lua
@@ -1,14 +1,37 @@
+-- arrow format stolen from entropy
+function FormatArrowMult(arrows, mult)
+    mult = number_format(mult)
+    if to_big(arrows) < to_big(-1) then 
+        return "="..mult 
+    elseif to_big(arrows) < to_big(0) then 
+        return "+"..mult 
+    elseif to_big(arrows) < to_big(6) then 
+        if to_big(arrows) < to_big(1) then
+            return "X"..mult
+        end
+        local arr = ""
+        for i = 1, to_big(arrows):to_number() do
+            arr = arr.."^"
+        end
+        return arr..mult
+    else
+        return "{"..arrows.."}"..mult
+    end
+end
+
 SMODS.Joker{ --The Fungus
     key = "thefungus",
     config = {
-        extra = 1
+        extra = {
+            operator = -1
+        }
     },
     loc_txt = {
         ['name'] = 'The Fungus',
         ['text'] = {
             [1] = 'Destroy a random {C:attention}Joker{} when {C:attention}Blind{} is selected',
-            [2] = 'and add {C:attention}twice{} the Joker\'s {C:attention}sell value{} to {X:dark_edition,C:white}operator{}',
-            [3] = '(Currently {X:dark_edition,C:white}#1##2##3#5{} Chips)',
+            [2] = 'and add the Joker\'s {C:attention}sell value{} to {X:dark_edition,C:white}operator{}',
+            [3] = '(Currently {X:dark_edition,C:white}#1#{} Chips)',
             [4] = '{s:0.8,C:inactive}wait this is NOT the expert{}',
             [5] = '{C:inactive}Art by 1.2m^2 Fungus Room{}'
         },
@@ -35,9 +58,7 @@ SMODS.Joker{ --The Fungus
     loc_vars = function(self, q, card)
         return {
             vars = {
-                "{",
-                card.ability.extra,
-                "}",
+                {FormatArrowMult(card.ability.extra.operator, 5)
             }
         }
     end,
@@ -55,8 +76,8 @@ SMODS.Joker{ --The Fungus
                 
                 if target_joker then
                     local joker_sell_value = target_joker.sell_cost or 0
-                    local sell_value_gain = joker_sell_value * 2
-                    card.ability.extra = card.ability.extra + sell_value_gain
+                    local sell_value_gain = joker_sell_value
+                    card.ability.extra.operator = card.ability.extra.operator + sell_value_gain
                     target_joker.getting_sliced = true
                     G.E_MANAGER:add_event(Event({
                         func = function()
@@ -70,19 +91,31 @@ SMODS.Joker{ --The Fungus
                 end
                 }
         end
-        if context.cardarea == G.jokers and context.joker_main  then
-          if to_big(card.ability.extra) < to_big(2) then
-            return {
-                echips = 5
-            }
-          else
-            return {
-                hyperchips = {
-                  math.floor(lenient_bignum(card.ability.extra)),
-                  5
-                }
-             }
-           end
+        if context.cardarea == G.jokers and context.joker_main or context.forcetrigger then
+			if to_big(card.ability.extra.operator) <= to_big(-1) then
+				return {
+					chips = 5
+				}
+			elseif to_big(card.ability.extra.operator) == to_big(0) then
+				return {
+                    x_chips = 5
+				}
+			elseif to_big(card.ability.extra.operator) == to_big(1) then
+				return {
+					echips = 5
+				}
+			elseif to_big(card.ability.extra.operator) == to_big(2) then
+				return {
+					eechips = 5
+				}
+			elseif to_big(card.ability.extra.operator) >= to_big(3) then
+				return {
+					hyperchips = {
+						lenient_bignum(card.ability.extra.operator),
+						5
+					}
+				}
+			end
         end
     end
 }
